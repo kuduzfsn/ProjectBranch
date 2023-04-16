0cfa5fc717fe5dc361993edf4d52906ac121dcb0
elkha <elkha@10.0.0.79>
2023-04-15 20:37:26 -0700

    new changes

--- src/test/java/branching/DemoBranching.java
+++ src/test/java/branching/DemoBranching.java
@@ -1,8 +1,40 @@
 package test.java.branching;
 
 public class DemoBranching {
+	
+	
+	
+	// For stash Purpose
+	
+	
+	public static void  changeToString(int a) {
+		
+		
+		String  str = Integer.toString(a);
+	
+		String newr = str.concat("123");
+		
+		System.out.println(newr);
+		//String lstr =newr.toLowerCase(null);
+		
+	
+		String jam = "AHMAD";
+		
+		String rrr =jam.toLowerCase();
+		
+		System.out.println(rrr);
+	}
 
+	public static void main(String[] args) {
+		
+		
+		changeToString(123);
+		
+				
+		
+	}
 }
 // Adding new comments
 
 // Second Comments
+
